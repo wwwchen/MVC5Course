a1ee5954b476a13fd956e0fe7c188933190f3797
max <wwwchen4@gmail.com>
2017-05-06 13:32:21 +0800

    09 練習在 Product 資料模型加入更多的驗證屬性

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -13,7 +13,7 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
-    
+
     public partial class Product
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,18 +21,24 @@ namespace MVC5Course.Models
         {
             this.OrderLine = new HashSet<OrderLine>();
         }
-    
+
         public int ProductId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "商品名稱必填")]
+        [MinLength(3), MaxLength(30)]
+        [RegularExpression("(.+)-(.+)", ErrorMessage = "商品名稱格式錯誤")]
         public string ProductName { get; set; }
+
         [Required]
+        [Range(0, 9999, ErrorMessage = "請設定正確的商品價格範圍")]
         public Nullable<decimal> Price { get; set; }
+
         [Required]
         public Nullable<bool> Active { get; set; }
+
         [Required]
         public Nullable<decimal> Stock { get; set; }
-    
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<OrderLine> OrderLine { get; set; }
     }
