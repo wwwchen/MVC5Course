812654cb361905c3726f4fdddafd3daf25743378
max <wwwchen4@gmail.com>
2017-05-07 14:00:43 +0800

    示範不同的計算導覽屬性資料總數的方法與效能差異

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -5,24 +5,31 @@ namespace MVC5Course.Models
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    
+
     [MetadataType(typeof(ProductMetaData))]
     public partial class Product
     {
         [DisplayName("訂單數量")]
         public virtual int OrderCount //若沒延遲載入，會有問題(目前沒有是因為前面controller 的 all 是用 AsQueryable，這個有延遲載入)
         {
-            get { return this.OrderLine.Count; }
+            get
+            {
+                return this.OrderLine.Count;
+                //return this.OrderLine.Count;
+                //return this.OrderLine.Where(p => p.Qty > 400).Count;
+                //return this.OrderLine.Where(p => p.Qty > 400).ToList().Count;
+                //return this.OrderLine.Count(p => p.Qty > 400);
+            }
         }
     }
-    
+
     public partial class ProductMetaData
     {
         [Required]
         public int ProductId { get; set; }
-        
+
         [DisplayName("商品名稱")]
-        [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        [StringLength(80, ErrorMessage = "欄位長度不得大於 80 個字元")]
         public string ProductName { get; set; }
 
         [DisplayName("商品價格")]
@@ -34,7 +41,7 @@ namespace MVC5Course.Models
 
         [DisplayName("商品庫存")]
         public Nullable<decimal> Stock { get; set; }
-    
+
         public virtual ICollection<OrderLine> OrderLine { get; set; }
     }
 }
