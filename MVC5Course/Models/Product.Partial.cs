8e32432b94d463bc5db45656734844943fdc67e4
max <wwwchen4@gmail.com>
2017-05-07 15:47:33 +0800

    21 練習用 try/catch 抓取 DbEntityValidationException 確切的錯誤訊息 part 2 會出現錯誤的

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -30,6 +30,8 @@ namespace MVC5Course.Models
 
         [DisplayName("商品名稱")]
         [StringLength(80, ErrorMessage = "欄位長度不得大於 80 個字元")]
+        //[MinLength(3), MaxLength(30)]
+        //[RegularExpression("(.+)-(.+)", ErrorMessage = "商品名稱格式錯誤")]
         public string ProductName { get; set; }
 
         [DisplayName("商品價格")]
